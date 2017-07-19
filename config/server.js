c73c55e8b6d76431bc5bca98055dc8ea0cf8eb99
Samuli De Pascale <samuli.depascale@aalto.fi>
2017-07-19 14:38:20 +0300

    added logging into server conf

--- config/server.js
+++ config/server.js
@@ -1,9 +1,24 @@
+const logger = require('./logger').logger
+
+logger.info(`Starting app with '${process.env.NODE_ENV}' environment.`)
+
 module.exports = app => {
-    if (process.env.NODE_ENV === 'test') {
-        return app.listen(4000)
-    } else if (process.env.NODE_ENV === 'production') {
-        return app.listen(3001)
-    } else {
-        return app.listen(3001)
+    try {
+        if (process.env.NODE_ENV === 'test') {
+            const server = app.listen(4000)
+            logger.info(`Listening on port 4000`)
+            return server
+        } else if (process.env.NODE_ENV === 'production') {
+            const server = app.listen(3001)
+            logger.info(`Listening on port 3001`)        
+            return server
+        } else {
+            const server = app.listen(3001)
+            logger.info(`Listening on port 3001`)        
+            return server
+        }
+    } catch (err) {
+        logger.fatal(`Aborting! Couldn't start server: ${err}`)
+        process.abort()
     }
 }
