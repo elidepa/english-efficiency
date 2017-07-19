e73de7f33ed32fa4dc3d6f54f86b5ca6ebf8c0d2
Samuli De Pascale <samuli.depascale@aalto.fi>
2017-07-19 13:57:15 +0300

    upgraded to production conf

--- config/db.js
+++ config/db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 const models = require('../models')
 
+const connectionStr = process.env.NODE_ENV === 'production' ? 'mongodb://ee:x7gdSYfJyRsWhe4@localhost/english-efficiency' : 'mongodb://localhost/test'
+
 module.exports = async app => {
     mongoose.Promise = require('bluebird')
-    mongoose.connect('mongodb://localhost/test')
+    mongoose.connect(connectionStr)
 }
--- config/server.js
+++ config/server.js
@@ -1,7 +1,9 @@
 module.exports = app => {
     if (process.env.NODE_ENV === 'test') {
         return app.listen(4000)
+    } else if (process.env.NODE_ENV === 'production') {
+        return app.listen(3001)
     } else {
-        return app.listen(3000)
+        return app.listen(3001)
     }
 }
